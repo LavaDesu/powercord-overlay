4e67dfb522a7da2cb89246e03cd87361c295f102
LavaDesu <me@lava.moe>
2021-08-20 00:23:47 +0700

    chore(style): minor formatting

--- drvs/discord.nix
+++ drvs/discord.nix
@@ -2,7 +2,7 @@
 , inputs
 , powercord
 }:
-discord-canary.overrideAttrs(old: {
+discord-canary.overrideAttrs (old: {
   installPhase = old.installPhase + ''
     cp -r ${inputs.self}/plugs $out/opt/DiscordCanary/resources/app
     substituteInPlace $out/opt/DiscordCanary/resources/app/index.js --replace 'POWERCORD_SRC' '${powercord}/libexec/powercord/deps/powercord'
--- drvs/powercord.nix
+++ drvs/powercord.nix
@@ -1,11 +1,10 @@
-{
-  inputs
+{ inputs
 , mkYarnPackage
 }:
 (mkYarnPackage {
-    name = "powercord";
-    src = inputs.powercord;
-    yarnLock = "${inputs.self}/misc/yarn.lock";
+  name = "powercord";
+  src = inputs.powercord;
+  yarnLock = "${inputs.self}/misc/yarn.lock";
 
-    patches = [ ../misc/powercord.patch ];
-}).overrideAttrs(_: { doDist = false; })
+  patches = [ ../misc/powercord.patch ];
+}).overrideAttrs (_: { doDist = false; })
