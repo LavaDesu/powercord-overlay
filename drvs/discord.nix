76386132b47c908928f19cbcee1edc0889a2f8bb
LavaDesu <me@lava.moe>
2021-08-20 11:13:34 +0700

    fix(discord): set pname to discord-plugged

--- drvs/discord.nix
+++ drvs/discord.nix
@@ -3,6 +3,7 @@
 , powercord
 }:
 discord-canary.overrideAttrs (old: {
+  pname = "discord-plugged";
   installPhase = old.installPhase + ''
     cp -r ${inputs.self}/plugs $out/opt/DiscordCanary/resources/app
     substituteInPlace $out/opt/DiscordCanary/resources/app/index.js --replace 'POWERCORD_SRC' '${powercord}/libexec/powercord/deps/powercord'
