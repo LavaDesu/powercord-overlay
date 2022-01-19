18beba3c5ac268cb8e8344c195b0a6b262cb0a99
LavaDesu <me@lava.moe>
2022-01-20 00:26:24 +0700

    refactor(powercord): use runCommandLocal instead

--- drvs/powercord.nix
+++ drvs/powercord.nix
@@ -1,45 +1,38 @@
 { lib
 , powercord-unwrapped
-, stdenvNoCC
+, runCommandLocal
 , plugins
 , themes
 }:
-stdenvNoCC.mkDerivation {
-  name = "powercord";
-  src = powercord-unwrapped.out;
+let
+  readName = file: lib.strings.sanitizeDerivationName (builtins.fromJSON (builtins.readFile file)).name;
 
-  installPhase =
-    let
-      readName = file: lib.strings.sanitizeDerivationName (builtins.fromJSON (builtins.readFile file)).name;
+  intoAddons = list: manifestName: builtins.map (element:
+  let
+    # We're relying on nix to coerce this into something we can use
+    path = "${element}";
+  in {
+    inherit path;
+    name = readName "${path}/${manifestName}";
+  }) list;
 
-      intoAddons = list: manifestName: builtins.map (element:
-      let
-        # We're relying on nix to coerce this into something we can use
-        path = "${element}";
-      in {
-        inherit path;
-        name = readName "${path}/${manifestName}";
-      }) list;
+  map = type: addons: lib.concatMapStringsSep "\n" (addon: ''cp -a "${addon.path}" "./${type}/${addon.name}"'') addons;
 
-      map = type: addons: lib.concatMapStringsSep "\n" (addon: ''
-        chmod 755 $out/src/Powercord/${type}
-        cp -a ${addon.path} $out/src/Powercord/${type}/${addon.name}
-        chmod -R u+w $out/src/Powercord/${type}/${addon.name}
-      '') addons;
-
-      mappedPlugins = map "plugins" (intoAddons plugins "manifest.json");
-      mappedThemes = map "themes" (intoAddons themes "powercord_manifest.json");
-    in ''
-      cp -a $src $out
-      chmod 755 $out
-      ln -s ${powercord-unwrapped.deps}/node_modules $out/node_modules
-
-      ${mappedPlugins}
-      ${mappedThemes}
-    '';
-
+  mappedPlugins = map "plugins" (intoAddons plugins "manifest.json");
+  mappedThemes = map "themes" (intoAddons themes "powercord_manifest.json");
+in
+runCommandLocal "powercord" {
   passthru.unwrapped = powercord-unwrapped;
   meta = powercord-unwrapped.meta // {
     priority = (powercord-unwrapped.meta.priority or 0) - 1;
   };
-}
+} ''
+  cp -a "${powercord-unwrapped}" "$out"
+  chmod u+w "$out"
+  ln -s "${powercord-unwrapped.deps}/node_modules" "$out/node_modules"
+
+  cd "$out/src/Powercord"
+  chmod u+w ./plugins ./themes
+  ${mappedPlugins}
+  ${mappedThemes}
+''
